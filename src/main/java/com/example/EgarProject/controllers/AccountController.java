6e0c8ac3a220074a61bed243f471566709f7c1cf
Nezhit <92212509+Nezhit@users.noreply.github.com>
2023-10-27 18:58:49 +0300

    удалил коммент в контроллере

--- src/main/java/com/example/EgarProject/controllers/AccountController.java
+++ src/main/java/com/example/EgarProject/controllers/AccountController.java
@@ -1,29 +1,22 @@
 package com.example.EgarProject.controllers;
 
 import com.example.EgarProject.models.ChangeJournal;
-import com.example.EgarProject.models.Task;
-import com.example.EgarProject.models.TaskCon;
 import com.example.EgarProject.models.User;
-import com.example.EgarProject.models.enums.ETaskCon;
 import com.example.EgarProject.pojo.ChangeConRequest;
-import com.example.EgarProject.repos.ChangeJournalRepo;
-import com.example.EgarProject.repos.TaskConRepo;
-import com.example.EgarProject.repos.TaskRepo;
 import com.example.EgarProject.services.ChangeConService;
 import com.example.EgarProject.services.UserInfo;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import java.time.LocalDateTime;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Controller
 
@@ -73,45 +66,8 @@ public class AccountController {
         model.addAttribute("user",user);
         changeConService.fixChanges(changeConRequest,user);
 
-        /*Optional<Task> oneTask = taskRepo.findById(changeConRequest.getTaskId());
-        System.out.println(oneTask.stream().iterator().next().getDescription());
-        if (oneTask.isPresent() && user.isPresent()) {
-            Task task = oneTask.get();
-            User currentUser = user.get();
-
-            Set<TaskCon> taskCons = new HashSet<>();
-
-            switch (changeConRequest.getSectionId()) {
-                case "section1":
-
-                    taskCons.add(taskConRepo.findByCondition(ETaskCon.TODO).get());
-                    break;
-                case "section2":
-                    taskCons.add(taskConRepo.findByCondition(ETaskCon.IN_PROGRESS).get());
-                    break;
-                case "section3":
-                    taskCons.add(taskConRepo.findByCondition(ETaskCon.TESTING).get());
-                    break;
-                case "section4":
-                    taskCons.add(taskConRepo.findByCondition(ETaskCon.DONE).get());
-                    break;
-            }
-
-            task.setTaskCon(taskCons);
-
-            ChangeJournal changeJournal = new ChangeJournal(task, currentUser, LocalDateTime.now(), changeConRequest.getTextInput());
-
-            // Сохраните объект ChangeJournal в базе данных
-            changeJournalRepo.save(changeJournal);
-        } else {
-            // Обработка ситуации, когда задача или пользователь не найдены в базе данных
-        }*/
 
 
-        //changeJournalRepo.saveAndFlush(changeJournal);
-        //  model.addAttribute("user",user);
-       // List<ChangeJournal> tasks= userInfo.getUserTasksByUsername(request);
-       // model.addAttribute("tasks",tasks);
         Map<Long, List<ChangeJournal>> groupedTasks=userInfo.getMapUserTasksByUsername(request);
         model.addAttribute("groupedTasks",groupedTasks);
         return "account";
